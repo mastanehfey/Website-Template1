b5dcd6aee991307601dd8d2d84e206416269edaa
Masti <mastaneh.f@gmail.com>
2020-04-16 12:50:38 -0700

    mobile menu done

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,14 @@
 var upBtn = document.querySelector('.up-button');
 var menuSection = document.querySelector('.menu-section');
 var headerHeight = document.getElementById('header').offsetHeight;
+var slideMenuOpen = false;
 
 window.addEventListener('scroll', (e)=> {
     moveUpBtn(e);
     menuSticky(e);    
-    console.log(window.innerWidth);
-    
+    if (slideMenuOpen){
+        closeMobileMenu();
+    }
 });
 //adjust up btn at the buttom of the page while scrolling
 function moveUpBtn(e){
@@ -17,18 +19,40 @@ function moveUpBtn(e){
 //menu sticky to the top
 function menuSticky(e){
     let scrollFromTop = e.target.documentElement.scrollTop;
-    
-    if (scrollFromTop >= 400) {
+    let headerImage;
+
+    headerImage = window.innerWidth > 992 ? 400 : 250;
+    if (scrollFromTop >= headerImage) {
         menuSection.classList.add('sticky');
         document.getElementById('home').style.paddingTop = '63px';
     }
-    else if(scrollFromTop < 400) {
+    else if (scrollFromTop < headerImage) {
         menuSection.classList.remove('sticky');
         document.getElementById('home').style.paddingTop = 0;
     }
 }
+//show menu on small screen size----------------------------------------------------------
+var bars = document.querySelector('.bars');
+var slideMenu = document.querySelector('.menu-list-slide');
+bars.addEventListener('click', openMobileMenu);
 
-//smooth scroll on menu click
+function openMobileMenu(){
+    if (slideMenuOpen) {
+        closeMobileMenu();
+    }
+    else{
+        //this needs to change if more items added to the slide menu
+        slideMenu.style.transition = 'ease-in .4s';
+        slideMenu.style.height = '150px';
+        slideMenuOpen = true;  
+    }  
+}
+function closeMobileMenu(){
+    slideMenu.style.height = 0;
+    slideMenu.style.transition = 'ease-in .2s';
+    slideMenuOpen = false;
+}
+//smooth scroll on menu click------------------------------------------------------------
 var menuItems = document.querySelectorAll('.menu-item-js');
 menuItems.forEach(item => {
     item.addEventListener('click', function (e) {
@@ -44,6 +68,10 @@ menuItems.forEach(item => {
         item.classList.add('active');
         }
         scrollToSection(link);
+
+        if (slideMenuOpen) {
+            closeMobileMenu();
+        }
     })
 })
 
