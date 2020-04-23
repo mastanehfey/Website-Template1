32f20764a086360f757066eb6100ecc657936015
Mastaneh <mastaneh.f@gmail.com>
2020-04-23 09:45:53 -0700

    Compatible with edge!

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,11 @@
+"use strict";
 var upBtn = document.querySelector('.up-button');
 var menuSection = document.querySelector('.menu-section');
 var headerHeight = document.getElementById('header').offsetHeight;
 var slideMenuOpen = false;
 
 window.addEventListener('scroll', (e)=> {
+    
     moveUpBtn(e);
     menuSticky(e);    
     if (slideMenuOpen){
@@ -12,36 +14,40 @@ window.addEventListener('scroll', (e)=> {
 });
 //adjust up btn at the buttom of the page while scrolling
 function moveUpBtn(e){
-    let scrolTop = e.target.documentElement.scrollTop;
+    let scrolTop = scrollY;
     let clientHeight = e.target.documentElement.clientHeight;
     upBtn.style.top = (clientHeight + scrolTop - 65) + 'px';
 }
 //menu sticky to the top
 function menuSticky(e){
-    let scrollFromTop = e.target.documentElement.scrollTop;
+    let scrollFromTop = scrollY;
     let headerImage;
 
     headerImage = window.innerWidth > 992 ? 400 : 250;
+    
     if (scrollFromTop >= headerImage) {
         menuSection.classList.add('sticky');
         document.getElementById('home').style.paddingTop = '63px';
     }
-    else if (scrollFromTop < headerImage) {
+    else if (scrollFromTop < headerImage) {        
         menuSection.classList.remove('sticky');
         document.getElementById('home').style.paddingTop = 0;
     }
 }
+
 //show menu on small screen size----------------------------------------------------------
 var bars = document.querySelector('.bars');
 var slideMenu = document.querySelector('.menu-list-slide');
 bars.addEventListener('click', openMobileMenu);
 
 function openMobileMenu(){
+    
     if (slideMenuOpen) {
         closeMobileMenu();
     }
     else{
         //this needs to change if more items added to the slide menu
+        
         slideMenu.style.transition = 'ease-in .4s';
         slideMenu.style.height = '150px';
         slideMenuOpen = true;  
@@ -75,14 +81,21 @@ menuItems.forEach(item => {
     })
 })
 
-function scrollToSection(id) {
-    let sectionTop = document.getElementById(id).offsetTop;
+function scrollToSection(id) {    
+    let sectionTop = document.getElementById(id).offsetTop;    
     let menuHeight = menuSection.getBoundingClientRect().height;
-
+    // smooth scrolling using jQuery
     if(id == 'home'){
-        document.documentElement.scrollTop = headerHeight;
+        $(document.scrollingElement).animate({
+            scrollTop: headerHeight}, 400);
     }
     else{
-        document.documentElement.scrollTop = sectionTop - menuHeight;        
+        $(document.scrollingElement).animate({
+            scrollTop: sectionTop - menuHeight}, 400);
     }
 }
+//scroll to top Up BUTTON on click
+upBtn.addEventListener('click',function(){
+    $(document.scrollingElement).animate({
+        scrollTop: 0}, 400);
+})
